a4427a065613526f3235f92c2eac5d6735904199
r00tm4ch1n3 <105373205+r00tm4ch1n3@users.noreply.github.com>
2023-12-14 01:28:46 +0300

    Create Homework3.sql

---
+++ Database/Relational/sql/Exercise/Homework3.sql
@@ -0,0 +1,13 @@
+--Odev3
+--1)
+-- select country from country
+-- where country like 'A%a';
+--2)
+-- select country from country
+-- where country ilike '_____%n';
+--3)
+-- select title from film
+-- where title ilike '%t%t%t%t%'
+--4)
+-- select * from film
+-- where title ilike 'C%' and length > 90 and rental_rate =2.99;
