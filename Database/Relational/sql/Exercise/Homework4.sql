f7757dc2799e1c288e8fc6d825dae8283a782f1c
r00tm4ch1n3 <105373205+r00tm4ch1n3@users.noreply.github.com>
2023-12-14 01:58:35 +0300

    Create Homework4.sql

---
+++ Database/Relational/sql/Exercise/Homework4.sql
@@ -0,0 +1,15 @@
+--Odev4
+--1)
+-- select distinct replacement_cost from film;
+
+--2)
+-- select count(distinct replacement_cost) from film;
+--3)
+-- select count(title) from film
+-- where title like'T%' and rating='G';
+--4)
+-- select count(country) from country
+-- where country ilike '_____';
+---5)
+-- select city from city
+-- where city ~~* 'r%';
