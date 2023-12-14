be2a75cbc3c7d5f8df1912084496743cab5e85c0
r00tm4ch1n3 <105373205+r00tm4ch1n3@users.noreply.github.com>
2023-12-14 21:43:49 +0300

    Create Homework5.sql

---
+++ Database/Relational/sql/Exercise/Homework5.sql
@@ -0,0 +1,17 @@
+--ODEV 5
+--1)
+-- select * from film
+-- where title like '%n'
+-- order by length DESC
+-- limit 5
+--2)
+-- select title,length from film
+-- where title like '%n'
+-- order by length
+-- offset 5
+-- limit 5
+--3)
+-- select last_name from customer
+-- where store_id=1
+-- order by last_name DESC
+-- limit 4
