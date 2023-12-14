4ba6fe4eaa361044ed82b3bfe597ec02ff4fc4d1
r00tm4ch1n3 <105373205+r00tm4ch1n3@users.noreply.github.com>
2023-12-14 22:06:17 +0300

    Create Homework6.sql

---
+++ Database/Relational/sql/Exercise/Homework6.sql
@@ -0,0 +1,12 @@
+-- ODEV6
+--1)
+-- select SUM(rental_rate) as ortalama from film;
+--2)
+-- select count(title) from film
+-- where title like 'C%'
+--3)
+-- select max(length) from film
+-- where rental_rate=0.99
+--4)
+-- select count(distinct replacement_cost) from film
+-- where length > 150 
